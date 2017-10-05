d73fc54d89183befc0fd210270c132f3692f8a1e
github username <kevinladkins@gmail.com>
2017-10-05 17:42:49 -0400

    Build structure for app

--- src/components/home.js
+++ src/components/home.js
@@ -4,7 +4,9 @@ class Home extends Component {
   render() {
     return (
       <div className="container text-center">
-      <h3>Home Page</h3>
+        <div className="row content">
+        </div>
+        <h3>Home Page</h3>
       </div>
     )
   }
