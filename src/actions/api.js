63002338af7732a6f329ba983e8ed5665cbfa9ee
github username <kevinladkins@gmail.com>
2017-10-05 15:10:12 -0400

    Set up API variables

--- src/actions/api.js
+++ src/actions/api.js
@@ -1,2 +1,4 @@
-export const API_KEY='f6731f51774e4af5900c6b23e4140752';
-export const ROOT_PATH='http://api.nytimes.com/svc/topstories/v2/';
+export const API_KEY = '?api-key=f6731f51774e4af5900c6b23e4140752';
+export const ROOT_PATH = 'http://api.nytimes.com/svc/topstories/v2/';
+
+export const SECTIONS = ['home', 'opinion', 'world', 'national', 'politics', 'upshot', 'nyregion', 'business', 'technology', 'science', 'health', 'sports', 'arts', 'books', 'movies']
