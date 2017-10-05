8eb250db0064cd86fe19d95488041a8456e8e334
github username <kevinladkins@gmail.com>
2017-10-05 17:39:02 -0400

    Restructure page

--- src/components/home.js
+++ src/components/home.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 class Home extends Component {
   render() {
     return (
+      <div className="container text-center">
       <h3>Home Page</h3>
+      </div>
     )
   }
 
