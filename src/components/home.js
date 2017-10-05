ab1db034463ebfd8e5ecd2b2ad57a3944c0d8669
github username <kevinladkins@gmail.com>
2017-10-05 18:24:38 -0400

    Add StoryDetail and StoriesINdex

--- src/components/home.js
+++ src/components/home.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import StoryDetail from '../containers/story_detail';
+import StoriesIndex from '../containers/stories_index';
 
 class Home extends Component {
   render() {
+    var { section } = this.props.match.params;
+    if (!section) { section = "home" }
+    
     return (
       <div className="container text-center">
         <div className="row content">
+          <StoryDetail />
+          <StoriesIndex />
         </div>
-        <h3>Home Page</h3>
       </div>
     )
   }
