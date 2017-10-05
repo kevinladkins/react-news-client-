67c3d96d0b6c1a4c7823f8ca42ea54770910db82
github username <kevinladkins@gmail.com>
2017-10-05 17:36:21 -0400

    Build navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,1 +1,43 @@
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { SECTIONS } from '../actions/api'
+
+const buildLinks = (name) => {
+  var routeName = name[0].toUpperCase() + name.substring(1)
+  return (
+    <li
+     className={window.location.pathname == `/${name}` ? 'active' : ''}
+    ><Link to={`${name}`}>{routeName}</Link></li>
+  )
+}
+
+const ROUTES = ['sports', 'weather']
+
+class Navbar extends Component {
+
+  render() {
+    console.log(window.location.pathname.split('/'))
+    return (
+      <nav className="navbar navbar-default" >
+        <div className="container">
+          <div className="navbar-header">
+            <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            </button>
+
+          </div>
+          <div className="collapse navbar-collapse" id="myNavbar">
+            <ul className="nav navbar-nav">
+              {SECTIONS.map(route => buildLinks(route))}
+            </ul>
+          </div>
+        </div>
+      </nav>
+    )
+  }
+
+}
+
+export default Navbar;
