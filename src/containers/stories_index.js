274db411a56b0566c67627596cfd39f0c3092620
github username <kevinladkins@gmail.com>
2017-10-06 13:43:38 -0400

    Render stories from State

--- src/containers/stories_index.js
+++ src/containers/stories_index.js
@@ -1,20 +1,40 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import _ from 'lodash';
+
 
 class StoriesIndex extends Component {
+
+  listStory(story) {
+    return (
+      <li className="list-group-item story-item" key={story.title}>
+        <h5>{story.title}</h5>
+        <img src={story.multimedia[1] ? story.multimedia[1].url : 'src/images/news.jpg'} />
+      </li>
+    )
+  }
+
   render() {
+    const { stories } = this.props;
+
+    if (!stories) {
+      return <div>Loading...</div>
+    }
+
+
+
     return (
       <div className="col-sm-4">
        <ul className="list-group story-list">
-        <li className="list-group-item">Story 1</li>
-        <li className="list-group-item">Story 2</li>
-        <li className="list-group-item">Story 3</li>
-        <li className="list-group-item">Story 4</li>
-        <li className="list-group-item">Story 5</li>
-        
+          {_.map(stories, story => this.listStory(story))}
         </ul>
       </div>
     )
   }
 }
 
-export default StoriesIndex;
+function mapStateToProps({ stories }) {
+  return { stories }
+}
+
+export default connect(mapStateToProps)(StoriesIndex);
