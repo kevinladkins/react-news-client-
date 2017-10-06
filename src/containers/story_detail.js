2fcd30d2d900dba8df72a8b1d5949c0db7b14176
github username <kevinladkins@gmail.com>
2017-10-06 13:44:29 -0400

    Render detail from state

--- src/containers/story_detail.js
+++ src/containers/story_detail.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class StoryDetail extends Component {
   render() {
-    
+
+
+
+    const { stories, lead } = this.props
+
+    if(!stories) {
+      return <div>Loading...</div>
+    }
+
+    const story = stories[lead];
+    var lastIndex = story.multimedia.length -1;
+    const image = story.multimedia[lastIndex];
+
     return (
       <div className="col-sm-8 story-detail">
+        <a href={story.url} target="_blank">
         <div className="well detail-well">
-          <h2 >Top Story</h2>
+          <h3>{story.title}</h3>
+          <img src={image ? image.url : "src/images/news.jpg"} />
+          <br />
+          <p>{story.abstract}</p>
         </div>
+        </a>
       </div>
     )
   }
 }
 
-export default StoryDetail;
+function mapStateToProps({ stories, lead }) {
+  return { stories, lead }
+}
+
+export default connect(mapStateToProps)(StoryDetail);
