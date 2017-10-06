31e2de093a0877f6c19d0ec6c593568818ac6320
github username <kevinladkins@gmail.com>
2017-10-06 13:46:06 -0400

    Add FETCH_STORIES action

--- src/reducers/reducers_stories.js
+++ src/reducers/reducers_stories.js
@@ -1,6 +1,14 @@
 import { defaultState } from './default'
 import slug from 'slug';
+import { mapAndSlug } from '../utils'
+import { FETCH_STORIES } from '../actions'
 
-export default function storiesReducer(state = defaultState, action) {
+export default function storiesReducer(state = null, action) {
+  switch (action.type) {
+    case FETCH_STORIES:
+      const { results } = action.payload.data;
+      const keyMapped = mapAndSlug(results, 'title');
+      return keyMapped;
+  }
   return state;
 }
