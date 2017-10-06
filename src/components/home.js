d960987e7022cdcfa2cd18fc9f39e8a9529500c5
github username <kevinladkins@gmail.com>
2017-10-06 13:41:59 -0400

    Add fetchStories dispatch to componentDidMount

--- src/components/home.js
+++ src/components/home.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import StoryDetail from '../containers/story_detail';
 import StoriesIndex from '../containers/stories_index';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { fetchStories } from '../actions';
 
 class Home extends Component {
-  render() {
+
+  componentDidMount() {
     var { section } = this.props.match.params;
-    if (!section) { section = "home" }
-    
+    if (!section) { section = "home" };
+    this.props.fetchStories(section);
+  }
+
+
+
+  render() {
     return (
       <div className="container text-center">
         <div className="row content">
@@ -16,7 +25,10 @@ class Home extends Component {
       </div>
     )
   }
-
 }
 
-export default Home;
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchStories }, dispatch)
+}
+
+export default connect(null, mapDispatchToProps)(Home);
