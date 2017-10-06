28c9c69ec8addb3fdd07320f05e2e6fd2879eb28
github username <kevinladkins@gmail.com>
2017-10-06 13:45:18 -0400

    Connect leadStoryReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,15 +1,19 @@
 import { combineReducers, applyMiddleware, createStore } from 'redux';
 import storiesReducer from './reducers_stories';
-import promise from 'redux-promise';
+import leadStoryReducer from './reducers_lead_story';
+import ReduxPromise from 'redux-promise';
 
 const rootReducer = combineReducers(
-  {stories: storiesReducer}
+  {
+    stories: storiesReducer,
+    lead: leadStoryReducer
+  }
 )
 
 const store = createStore(
   rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(promise)
+  applyMiddleware(ReduxPromise)
 )
 
 export default store;
