0a86437f8f8a2507116f5a45e1eb9178bf9b1c6e
github username <kevinladkins@gmail.com>
2017-10-07 16:02:47 -0400

    Add updateStories action

--- src/components/home.js
+++ src/components/home.js
@@ -3,14 +3,28 @@ import StoryDetail from '../containers/story_detail';
 import StoriesIndex from '../containers/stories_index';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { fetchStories } from '../actions';
+import { fetchStories, fetchUpdate } from '../actions';
 
 class Home extends Component {
 
-  componentDidMount() {
+  updateStories() {
+    var { section } = this.props.match.params;
+    if (!section) { section = "home" };
+    this.props.fetchUpdate(section)
+  }
+
+  componentWillMount() {
     var { section } = this.props.match.params;
     if (!section) { section = "home" };
     this.props.fetchStories(section);
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(this.updateStories.bind(this), 10000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
   }
 
 
@@ -28,7 +42,7 @@ class Home extends Component {
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ fetchStories }, dispatch)
+  return bindActionCreators({ fetchStories, fetchUpdate }, dispatch)
 }
 
 export default connect(null, mapDispatchToProps)(Home);
